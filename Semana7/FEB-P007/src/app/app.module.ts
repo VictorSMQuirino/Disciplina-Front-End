fa36cd7323d23d78827aa18815b9a3bdc3dd0520
Víctor Quirino <victorsouzaq@gmail.com>
2024-01-14 18:04:05 -0300

    Instrução prática FEB-P007 - feat: Componente de botões das categorias de veículos

---
+++ Semana7/FEB-P007/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ListaCategoriasComponent } from './components/lista-categorias/lista-categorias.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaCategoriasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
