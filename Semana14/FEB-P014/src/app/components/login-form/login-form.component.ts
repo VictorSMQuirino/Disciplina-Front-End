597967314fda7487eccf0a76aadda9e022b72349
Víctor Quirino <victorsouzaq@gmail.com>
2024-03-07 16:37:32 -0300

    Correção no registro de novos usuários

--- Semana14/FEB-P014/src/app/components/login-form/login-form.component.ts
+++ Semana14/FEB-P014/src/app/components/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -11,7 +12,7 @@ export class LoginFormComponent implements OnInit {
 
   loginFormGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.loginFormGroup = this.formBuilder.group({
@@ -25,13 +26,20 @@ export class LoginFormComponent implements OnInit {
 
   submitLoginForm() {
     if(this.loginFormGroup.valid) {
-      this.authService.login(this.loginFormGroup.get('Email')?.value, this.loginFormGroup.get('Password')?.value);
+      this.authService.login(this.loginFormGroup.get('Email')?.value, this.loginFormGroup.get('Password')?.value).subscribe(response => {
+        console.log(response);
+        if(response.registered) { this.router.navigate(['home']) };
+      })
     }
   }
 
   signUpUser() {
+    const email = this.loginFormGroup.get('Email')?.value;
+    const password = this.loginFormGroup.get('Password')?.value;
     if(this.loginFormGroup.valid) {
-      this.authService.signupUser(this.loginFormGroup.get('Email')?.value, this.loginFormGroup.get('Password')?.value);
+      this.authService.signupUser(email, password).subscribe(response => {
+        console.log(response);
+      })
     }
   }
 
